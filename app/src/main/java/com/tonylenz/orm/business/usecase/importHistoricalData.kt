8008329cec720e68fe748f31a00d63bb6efd6ca3
Tony Lenz <tony@steadfastinnovation.com>
2024-04-28 13:57:06 -0700

    Add cancellation to the importHistoricalData use-case

--- app/src/main/java/com/tonylenz/orm/business/usecase/importHistoricalData.kt
+++ app/src/main/java/com/tonylenz/orm/business/usecase/importHistoricalData.kt
@@ -6,6 +6,9 @@ import com.github.michaelbull.result.Result
 import com.tonylenz.orm.business.parseHistoricalEntry
 import com.tonylenz.orm.data.HistoricalDataRepo
 import com.tonylenz.orm.model.WorkoutSet
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.ensureActive
 import okio.Source
 import okio.buffer
 
@@ -13,23 +16,27 @@ import okio.buffer
  * The import data use-case. Unspecified functional requirements around subsequent imports, I chose
  * to treat the application as a historical data file viewer. e.g. The app only allows you to see
  * the historical data from one file at a time.
+ *
+ * // TODO: Allow cancelling import in the UI
  */
-@Suppress("NAME_SHADOWING") // allow name shadowing to restrict arg scope
 suspend fun importHistoricalData(
     data: Source,
     repo: HistoricalDataRepo,
-): Result<Unit, Exception> {
-    return try {
+): Result<Unit, Exception> = coroutineScope{
+    try {
         data.buffer().use { data ->
             val dataList = mutableListOf<WorkoutSet>()
             var line = data.readUtf8Line()
             while (line != null) {
                 dataList.add(parseHistoricalEntry(line))
+                ensureActive() // Allow cancellation
                 line = data.readUtf8Line()
             }
             repo.saveWorkoutSets(dataList, replaceAll = true)
         }
         Ok(Unit)
+    } catch (e: CancellationException) {
+        throw e // Allow coroutine cancellation
     } catch (e: Exception) {
         Err(e)
     }
